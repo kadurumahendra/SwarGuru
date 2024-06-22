72ce0a569d203d6aea36e757e319bdbec17197c0
kadurumahendra <kadurumahendra1424.sse@saveetha.com>
2024-06-22 13:58:10 +0530

    initial commit

--- app/src/main/java/com/example/swarguru/ChangePasswordActivity.kt
+++ app/src/main/java/com/example/swarguru/ChangePasswordActivity.kt
@@ -5,11 +5,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.swarguru.databinding.ActivityProfileChangepasswordBinding
 
 class ChangePasswordActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProfileChangepasswordBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_profile_changepassword)
+        binding = ActivityProfileChangepasswordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
